f3dcee45ace0e4908c119476b237653cd5b88183
Eternal-Sloth <kyo.tokiwa97@gmail.com>
2023-09-14 13:46:02 +0700

    Authentication Controller

--- Childen Health Care System/src/java/AuthenticationController/BaseAuthenticationController.java
+++ Childen Health Care System/src/java/AuthenticationController/BaseAuthenticationController.java
@@ -4,10 +4,90 @@
  */
 package AuthenticationController;
 
+import Model.Patient;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  *
  * @author GL
  */
-public class BaseAuthenticationController {
+public abstract class BaseAuthenticationController {
+    private boolean isAuthenticated(HttpServletRequest request) {
+        Patient patient = (Patient) request.getSession().getAttribute("account");
+        return patient!= null;
+    }
     
+    protected abstract void doGet(HttpServletRequest request, HttpServletResponse response,Account acc)
+    throws ServletException, IOException; 
+    
+    protected abstract void doPost(HttpServletRequest request, HttpServletResponse response,Account acc)
+    throws ServletException, IOException; 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if(isAuthenticated(request)) {
+            Patient account = (Account)request.getSession().getAttribute("account");
+            doGet(request, response, account);
+        }
+        else {
+            response.getWriter().print("Access Denied!");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                if(isAuthenticated(request)) {
+            Account account = (Account)request.getSession().getAttribute("account");
+            doPost(request, response, account);
+        }
+        else {
+            response.getWriter().print("Access Denied!");
+        }
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
